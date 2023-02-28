a2dafafd479e7795e7e285595446a34ea1b0ecdb
ronitshinde <rony.shinde753@gmail.com>
2023-02-28 20:31:44 +0530

    [Ronit] Add : UC2-Validate last name with first capital letter

--- RegexProgram/RegexProgram/Program.cs
+++ RegexProgram/RegexProgram/Program.cs
@@ -11,21 +11,27 @@ namespace RegexProgram
         static void Main(string[] args)
         {
             bool flag = true;
-            while (flag) 
+            while (flag)
             {
                 Console.WriteLine("Enter the option number");
-                Console.WriteLine("1.Validate first name");
+                Console.WriteLine("1.Validate first name \n2.Validate last name \n0.Exit");
                 Console.WriteLine();
+                RegexProblem regex = new RegexProblem();
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
                     case 1:
-                        RegexProblem regex = new RegexProblem();
                         regex.ValidateFirstName("Phoenix");
+                        break;
+                    case 2:
+                        regex.ValidateLastName("Cruise");
+                        break;
+                    case 0:
+                        flag = false;
                         Console.ReadKey();
                         break;
                 }
-            }        
+            }
         }
     }
 }
--- RegexProgram/RegexProgram/RegexProblem.cs
+++ RegexProgram/RegexProgram/RegexProblem.cs
@@ -22,5 +22,19 @@ namespace RegexProgram
                 Console.WriteLine("First Name is not matching with Regex");
             }
         }
+        public void ValidateLastName(string name)
+        {
+            //Cruise
+            string lastName = "^[A - Z]{1}[a - z]{3,}?";
+            if (Regex.IsMatch(name, lastName))
+            {
+                Console.WriteLine("Last Name is matching with Regex");
+
+            }
+            else
+            {
+                Console.WriteLine("Last Name is not matching with Regex");
+            }
+        }
     }
 }
